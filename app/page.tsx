e30fcbea68ccf3cbf9089675342d678ba21216ab
s414june <s414june@gmail.com>
2023-07-09 14:18:53 +0800

    修改mock資料內容與抓取結構 調整手機板rem大小

--- app/page.tsx
+++ app/page.tsx
@@ -3,129 +3,10 @@
 import Image from "next/image";
 import SlideCard from "../components/slidecard";
 import { useEffect } from "react";
+import getSliderDatas from '../mock/sliderDatas'
 
-let slideDatas = [
-  {
-    name: "Gaming RDY SLMBG221",
-    type: "prebuilt",
-    description: `Windows 11 Home\n
-    Intel® Core™ i7-13700F CPU\n
-    GeForce RTX 4060 Ti - 8GB\n
-    1TB M.2 NVMe SSD\n
-    16GB DDR5-5200 RAM`,
-    count_sale: 1399,
-    count_origin: 1679,
-    affirm_mo: 87,
-    shipping: "Free Shipping",
-    delivery: "Delivery By Tuesday, Jul 11",
-    image_src: "/images/pc/pc_1.webp",
-  },
-  {
-    name: "Gaming RDY IWRG209",
-    type: "prebuilt",
-    description: `Windows 11 Home\n
-    AMD Ryzen 7 7700X CPU\n
-    GeForce RTX 4070 Ti - 12GB\n
-    2TB M.2 NVMe SSD\n
-    32GB DDR5-5600MHz RGB RAM`,
-    count_sale: 1399,
-    count_origin: 1679,
-    affirm_mo: 87,
-    shipping: "Free Shipping",
-    delivery: "Delivery By Tuesday, Jul 11",
-    image_src: "/images/pc/pc_2.webp",
-  },
-  {
-    name: "Intel 13th Gen Elite Daily Deal",
-    type: "custom",
-    description: `Windows 11 Home\n
-    Intel® Core™ i7-13700KF CPU\n
-    GeForce RTX 3080 - 10GB\n
-    1TB Crucial P3 M.2 NVMe SSD\n
-    32GB DDR5-6000MHz ADATA RGB RAM`,
-    count_sale: 1399,
-    count_origin: 1679,
-    affirm_mo: 87,
-    shipping: "Free Shipping",
-    delivery: "Delivery By Tuesday, Jul 11",
-    image_src: "/images/pc/pc_3.webp",
-  },
-  {
-    name: "AMD Ryzen 9 Extreme Gaming Daily Deal",
-    type: "custom",
-    description: `Windows 11 Home\n
-    AMD Ryzen™ 9 7950X3D CPU\n
-    GeForce RTX 4090 - 24GB\n
-    2TB Intel 670p M.2 NVMe SSD\n
-    32GB DDR5-5600MHz TEAMGROUP RGB RA`,
-    count_sale: 1399,
-    count_origin: 1679,
-    affirm_mo: 87,
-    shipping: "Free Shipping",
-    delivery: "Delivery By Tuesday, Jul 11",
-    image_src: "/images/pc/pc_4.webp",
-  },
-  {
-    name: "Gaming RDY SLHBG224",
-    type: "prebuilt",
-    description: `Windows 11 Home\n
-    Intel® Core™ i5-13400F CPU\n
-    GeForce RTX 3060 - 12GB\n
-    1TB M.2 NVMe SSD\n
-    16GB DDR5-5200 RAM`,
-    count_sale: 1399,
-    count_origin: 1679,
-    affirm_mo: 87,
-    shipping: "Free Shipping",
-    delivery: "Delivery By Tuesday, Jul 11",
-    image_src: "/images/pc/pc_5.webp",
-  },
-  {
-    name: "Gaming RDY SLMRR214",
-    type: "prebuilt",
-    description: `Windows 11 Home\n
-    AMD Ryzen 5 7600 CPU\n
-    AMD Radeon RX 7600 - 8GB\n
-    1TB M.2 NVMe SSD\n
-    16GB DDR5-5200 RAM`,
-    count_sale: 1399,
-    count_origin: 1679,
-    affirm_mo: 87,
-    shipping: "Free Shipping",
-    delivery: "Delivery By Tuesday, Jul 11",
-    image_src: "/images/pc/pc_6.webp",
-  },
-  {
-    name: "Intel 13th Gen Gaming PC Daily Deal",
-    type: "custom",
-    description: `Windows 11 Home\n
-    Intel® Core™ i5-13600KF CPU\n
-    GeForce RTX 3080 - 10GB\n
-    1TB Crucial P3 M.2 NVMe SSD\n
-    32GB DDR5-6000MHz ADATA RGB RAM`,
-    count_sale: 1399,
-    count_origin: 1679,
-    affirm_mo: 87,
-    shipping: "Free Shipping",
-    delivery: "Delivery By Tuesday, Jul 11",
-    image_src: "/images/pc/pc_7.webp",
-  },
-  {
-    name: "AMD Ryzen Power Up Daily Deal",
-    type: "custom",
-    description: `Windows 11 Home\n
-    AMD Ryzen™ 5 5600 CPU\n
-    GeForce RTX 3060 Ti - 8GB\n
-    500GB WD SN570 M.2 NVMe SSD\n
-    32GB DDR4-3600MHz ADATA RGB RAM`,
-    count_sale: 1399,
-    count_origin: 1679,
-    affirm_mo: 87,
-    shipping: "Free Shipping",
-    delivery: "Delivery By Tuesday, Jul 11",
-    image_src: "/images/pc/pc_8.webp",
-  },
-];
+let sliderDatas = getSliderDatas();
+
 export default function Home({ Component, pageProps }) {
   useEffect(() => {
     let cardOutside = document.querySelector(".card-outside");
@@ -199,7 +80,7 @@ export default function Home({ Component, pageProps }) {
         </div>
         <div className="w-full relative">
           <div className="overflow-x-auto card-outside whitespace-nowrap relative">
-            {slideDatas.map((slideData, i) => (
+            {sliderDatas.map((slideData, i) => (
               <SlideCard
                 data={slideData}
                 key={i}
---
+++ mock/sliderDatas.ts
@@ -0,0 +1,127 @@
+let getSliderDatas = ()=>{
+  let sliderDatas = [
+    {
+      name: "Gaming RDY SLHBG224",
+      type: "prebuilt",
+      description: `Windows 11 Home
+      Intel® Core™ i5-13400F CPU
+      GeForce RTX 3060 - 12GB
+      1TB M.2 NVMe SSD
+      16GB DDR5-5200 RAM`,
+      count_sale: 1149,
+      count_origin: 1399,
+      affirm_mo: 72,
+      shipping: "Free Shipping",
+      delivery: "Delivery By Wednesday, Jul 12",
+      image_src: "/images/pc/pc_1.webp",
+    },
+    {
+      name: "Gaming RDY SLHRG223",
+      type: "prebuilt",
+      description: `Windows 11 Home
+      AMD Ryzen 5 5600 CPU
+      GeForce RTX 4060 Ti - 8GB
+      1TB M.2 NVMe SSD
+      16GB DDR4-3200 RGB RAM`,
+      count_sale: 1199,
+      count_origin: 1399,
+      affirm_mo: 75,
+      shipping: "Free Shipping",
+      delivery: "Delivery By Wednesday, Jul 12",
+      image_src: "/images/pc/pc_2.webp",
+    },
+    {
+      name: "Intel 13th Gen Extreme Gaming PC Daily Deal",
+      type: "custom",
+      description: `Windows 11 Home
+      Intel® Core™ i9-13900KF CPU
+      GeForce RTX 4090 - 24GB
+      2TB Intel 670p M.2 NVMe SSD
+      32GB DDR5-6000MHz ADATA RGB RAM`,
+      count_sale: 3299,
+      count_origin: 3499,
+      affirm_mo: 160,
+      shipping: "Free Shipping",
+      delivery: "Estimate Ship By 07/14/2023",
+      image_src: "/images/pc/pc_3.webp",
+    },
+    {
+      name: "AMD Ryzen Pro Daily Deal",
+      type: "custom",
+      description: `Windows 11 Home
+      AMD Ryzen™ 7 7700X CPU
+      GeForce RTX 4070 Ti - 12GB
+      1TB Crucial P3 M.2 NVMe SSD
+      32GB DDR5-5600MHz TEAMGROUP RGB`,
+      count_sale: 1989,
+      count_origin: 2189,
+      affirm_mo: 96,
+      shipping: "Free Shipping",
+      delivery: "Estimate Ship By 07/14/2023",
+      image_src: "/images/pc/pc_4.webp",
+    },
+    {
+      name: "RDY Hakos Baelz Pro II",
+      type: "prebuilt",
+      description: `Windows 11 Home
+      Intel® Core™ i9-13900KF CPU
+      GeForce RTX 3080 10GB
+      2TB WD Black M.2 GEN4 NVMe SSD
+      32GB DDR5-5600 KINGSTON RGB RAM`,
+      count_sale: 2299,
+      count_origin: 2899,
+      affirm_mo: 111,
+      shipping: "Free Shipping",
+      delivery: "Delivery By Wednesday, Jul 12",
+      image_src: "/images/pc/pc_5.webp",
+    },
+    {
+      name: "Gaming RDY SLMRG211",
+      type: "prebuilt",
+      description: `Windows 11 Home
+      AMD Ryzen 7 7700X CPU
+      GeForce RTX 4070 - 12GB
+      1TB M.2 NVMe SSD
+      32GB DDR5-5600MHz RGB RAM`,
+      count_sale: 1799,
+      count_origin: 1999,
+      affirm_mo: 87,
+      shipping: "Free Shipping",
+      delivery: "Delivery By Wednesday, Jul 12",
+      image_src: "/images/pc/pc_6.webp",
+    },
+    {
+      name: "Intel 13th Gen Elite Daily Deal",
+      type: "custom",
+      description: `Windows 11 Home
+      Intel® Core™ i7-13700KF CPU
+      GeForce RTX 3080 - 10GB
+      1TB Crucial P3 M.2 NVMe SSD
+      32GB DDR5-6000MHz ADATA RGB RAM`,
+      count_sale: 2129,
+      count_origin: 2329,
+      affirm_mo: 103,
+      shipping: "Free Shipping",
+      delivery: "Estimate Ship By 07/14/2023",
+      image_src: "/images/pc/pc_7.webp",
+    },
+    {
+      name: "AMD Ryzen 9 Supreme Daily Deal",
+      type: "custom",
+      description: `Windows 11 Home
+      AMD Ryzen™ 9 7900X CPU
+      AMD Radeon RX 7900 XT - 20GB
+      1TB Crucial P3 M.2 NVMe SSD
+      32GB DDR5-5600MHz TEAMGROUP RGB`,
+      count_sale: 2199,
+      count_origin: 2399,
+      affirm_mo: 107,
+      shipping: "Free Shipping",
+      delivery: "Estimate Ship By 07/14/2023",
+      image_src: "/images/pc/pc_8.webp",
+    },
+  ];
+  return sliderDatas;
+}
+
+export default getSliderDatas
